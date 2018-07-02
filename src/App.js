07f67c93c5eb6c3d470effc48fb5a918f74b7469
SaravaiyaDhaval <dhavalsaravaiya@gmail.com>
2018-07-02 13:06:44 +0530

    complate

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src="https://openclipart.org/download/234331/Chromatic-Peacock-2-No-Background.svg" className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome To My Website</h1>
+        </header>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
